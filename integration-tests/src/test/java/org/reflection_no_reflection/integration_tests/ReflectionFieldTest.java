279ff4ba9c283dcbfbcfb83b85d1e764814549a0
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-11-30 12:50:26 -0500

    Make tests simpler

--- integration-tests/src/test/java/org/reflection_no_reflection/integration_tests/ReflectionFieldTest.java
+++ integration-tests/src/test/java/org/reflection_no_reflection/integration_tests/ReflectionFieldTest.java
@@ -1,6 +1,11 @@
 package org.reflection_no_reflection.integration_tests;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import javax.inject.Inject;
 import org.reflection_no_reflection.Field;
+import org.reflection_no_reflection.no_reflection.NoReflectionFieldFinder;
 import org.reflection_no_reflection.reflection.ReflectionField;
 
 /**
@@ -12,18 +17,12 @@ public class ReflectionFieldTest extends FieldTest {
 
     @Override
     public Field getField(Class<?> clazz, String fieldName) {
-        switch (fieldName) {
-            case "publicObjectField":
-                return new ReflectionField(getTrueReflectionFieldPublicObjectField());
-            case "publicPrimitiveIntField":
-                return new ReflectionField(getTrueReflectionFieldPublicPrimitiveIntField());
-            case "publicPrimitiveByteField":
-                return new ReflectionField(getTrueReflectionFieldPublicPrimitiveByteField());
-            case "publicPrimitiveShortField":
-                return new ReflectionField(getTrueReflectionFieldPublicPrimitiveShortField());
-            case "publicPrimitiveLongField":
-                return new ReflectionField(getTrueReflectionFieldPublicPrimitiveLongField());
+        List<java.lang.reflect.Field> allFields = Arrays.asList(clazz.getFields());
+        for (java.lang.reflect.Field field : allFields) {
+            if( field.getName().equals(fieldName)) {
+                return new ReflectionField(field);
+            }
         }
-        throw new RuntimeException("No such field:" + fieldName);
+        throw new RuntimeException("No such field " + fieldName);
     }
 }
