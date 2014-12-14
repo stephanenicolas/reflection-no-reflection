58f4375b336eba3e1e1e843ce3169d11af292987
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-12-14 15:53:27 -0500

    quite good documenation syste,

--- common-lib/src/main/java/org/reflection_no_reflection/package-info.java
+++ common-lib/src/main/java/org/reflection_no_reflection/package-info.java
@@ -1,25 +1,26 @@
 /**
  * See below, this document describes an overview of the
- * Reflection No Reflection library.
+ * common-library.
  *
- * Demonstration for PlantUML.
- * <p>
- * Example of use:
- * <p>
- * <img src="doc-files/image1.png">
+ * The common-lib provides high level classes used by RnR clients.
+ * Those high level classes have different implementations. One of them
+ * is based on the java core reflection library and the other one
+ * is based on RnR.
+ * <img src="doc-files/common-lib-overview.png">
  * @author SNI
  */
 /*
- * @startuml doc-files/image1.png
- * title Overview of Reflection No Reflection components
- * [common-lib]
- * [common-lib] <-down- [annotation-processor]: use
- * [common-lib] <-down- [weaver]: use
- * [gradle-plugin] -down-> [sample] : applied during build
- * [gradle-plugin] .right.> [weaver] : activate
- * [gradle-plugin] .left.> [annotation-processor] : activate
+ * @startuml doc-files/common-lib-overview.png
+ * title Overview of common-lib
+ * org.RnR.Field <|-- org.RnR.reflection.ReflectionField
+ * org.RnR.Field <|-- org.RnR.no_reflection.NoReflectionField
+ * org.RnR.FieldFinder <|-- org.RnR.reflection.ReflectionFieldFinder
+ * org.RnR.FieldFinder <|-- org.RnR.no_reflection.NoReflectionFieldFinder
+ * package org.RnR {
+ *   class Field
+ * note top : Provides the same API as java.lang.Field
+ *   class FieldFinder
+ * }
  * @enduml
- *
- * You can now browse each package to find more on the role of each component.
  */
 package org.reflection_no_reflection;
