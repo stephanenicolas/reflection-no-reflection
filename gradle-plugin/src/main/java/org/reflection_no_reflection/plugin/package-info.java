14132feec3404794d45f71378ee8c894ba8d95ee
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-12-14 21:19:41 -0500

    Better build cycle schema

--- gradle-plugin/src/main/java/org/reflection_no_reflection/plugin/package-info.java
+++ gradle-plugin/src/main/java/org/reflection_no_reflection/plugin/package-info.java
@@ -7,18 +7,22 @@
  * @startuml doc-files/build-cycle.png
  * skinparam activity {
  *   BackgroundColor<< Result >> Olive
+ *   BackgroundColor<< Anno >> Aquamarine
+ *   BackgroundColor<< Weaving >> Coral
  * }
  *
  * (*) --> "Java source files" << Result >>
  * "Java source files" << Result >> --> "Compile Java sources\n without annotation processing"
  * --> "Java class files" << Result >>
- * "Java source files" << Result >> --> "Compile Java sources\n with annotation processing only"
+ * "Java source files" << Result >> --> "Compile Java sources\n with annotation processing only" << Anno >>
  * --> "Annotation database source" << Result >>
  * "Annotation database source" --> "Compile annotation database only"
  * --> "Annotation database class file" << Result >>
- * "Java class files" << Result >> --> [in classpath] "Compile annotation database only"
+ * "Java class files" << Result >> --> "Weave byte code" << Weaving >>
+ * "Weave byte code" --> "Weaved Java class files" << Result >>
+ *  --> [in classpath] "Compile annotation database only"
  * "Annotation database class file" << Result >> --> "copy"
- * "Java class files" << Result >> --> "copy"
+ * "Weaved Java class files" << Result >> --> "copy"
  * "copy" --> "Java classes \n+ Annotation Database" << Result >>
  * "Java classes \n+ Annotation Database" << Result >> --> "dex"
  * "dex" --> "Dex file" << Result >>
