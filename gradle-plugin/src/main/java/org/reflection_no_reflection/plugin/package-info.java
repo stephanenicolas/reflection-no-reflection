3036c628ab75572bf27a7b8d24ae082883bef389
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-12-14 20:23:36 -0500

    Express build cycle with plantuml

--- gradle-plugin/src/main/java/org/reflection_no_reflection/plugin/package-info.java
+++ gradle-plugin/src/main/java/org/reflection_no_reflection/plugin/package-info.java
@@ -5,9 +5,23 @@
  */
 /*
  * @startuml doc-files/build-cycle.png
+ * skinparam activity {
+ *   BackgroundColor<< Result >> Olive
+ * }
  *
- * (*) --> "First Activity"
- * "First Activity" --> (*)
+ * (*) --> "Java source files" << Result >>
+ * "Java source files" << Result >> --> "Compile Java sources\n without annotation processing"
+ * --> "Java class files" << Result >>
+ * "Java source files" << Result >> --> "Compile Java sources\n with annotation processing only"
+ * --> "Annotation database source" << Result >>
+ * "Annotation database source" --> "Compile annotation database only"
+ * --> "Annotation database class file" << Result >>
+ * "Java class files" << Result >> --> [in classpath] "Compile annotation database only"
+ * "Annotation database class file" << Result >> --> "copy"
+ * "Java class files" << Result >> --> "copy"
+ * "copy" --> "Java classes \n+ Annotation Database" << Result >>
+ * "Java classes \n+ Annotation Database" << Result >> --> "dex"
+ * "dex" --> "Dex file" << Result >>
  * @enduml
  */
 package org.reflection_no_reflection.plugin;
