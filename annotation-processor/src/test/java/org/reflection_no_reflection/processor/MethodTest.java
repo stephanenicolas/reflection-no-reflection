c44d9202ee05bd9933d2473676dee63aacaca6d4
Stéphane Nicolas <steff.nicolas@gmail.com>
2015-05-20 17:43:34 -0700

    test params

--- annotation-processor/src/test/java/org/reflection_no_reflection/processor/MethodTest.java
+++ annotation-processor/src/test/java/org/reflection_no_reflection/processor/MethodTest.java
@@ -61,6 +61,47 @@ public class MethodTest {
         assertThat(method.getAnnotation(deprecatedAnnotationClass).annotationType(), is(deprecatedAnnotationClass));
     }
 
+    @Test
+    public void mapsSimpleAnnotatedMethodWithParams() throws ClassNotFoundException {
+        JavaFileObject source = JavaFileObjects.forSourceString("test.Foo", Joiner.on('\n').join( //
+                                                                                                  "package test;", //
+                                                                                                  "public class Foo {",//
+                                                                                                  "@Deprecated protected String s(String a) {return a; }", //
+                                                                                                  "}" //
+        ));
+
+        configureProcessor(new String[] {"java.lang.Deprecated"});
+        ASSERT.about(javaSource())
+            .that(source)
+            .processedWith(rnrProcessors())
+            .compilesWithoutError();
+
+        final Set<Class> annotatedClasses = processor.getAnnotatedClasses();
+        assertThat(annotatedClasses.contains(new Class("test.Foo")), is(true));
+
+        final Class expectedParamType = Class.forName("java.lang.String");
+        final Class aClass = Class.forName("test.Foo");
+        assertThat(aClass.getMethods().size(), is(1));
+
+        final Method method = (Method) aClass.getMethods().get(0);
+        final Method expected = new Method(aClass, "s", new Class[] {expectedParamType}, new Class("java.lang.String"), new Class[0], Modifier.PROTECTED);
+        assertThat(method, is(expected));
+        assertThat(method.getModifiers(), is(Modifier.PROTECTED));
+
+        final Annotation[] annotations = method.getDeclaredAnnotations();
+        assertThat(annotations.length, is(1));
+
+        final Class deprecatedAnnotationClass = Class.forName("java.lang.Deprecated");
+        assertThat(annotations[0].annotationType(), is(deprecatedAnnotationClass));
+        assertThat(method.getAnnotation(deprecatedAnnotationClass).annotationType(), is(deprecatedAnnotationClass));
+
+        final Class<?>[] paramTypes = method.getParameterTypes();
+        assertThat(paramTypes.length, is(1));
+        assertThat(paramTypes[0], is(expectedParamType));
+
+        assertThat(method.getAnnotation(deprecatedAnnotationClass).annotationType(), is(deprecatedAnnotationClass));
+    }
+
     private void configureProcessor(String[] annotations) {
         processor.setAnnotatedClasses(new HashSet<>(Arrays.asList(annotations)));
     }
